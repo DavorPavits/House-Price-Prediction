4c42819187e17a41b9ebc9e490949a6d19208f7b
DavorPavits <davorpavits@gmail.com>
2024-04-01 14:44:02 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#TODO 1:Analyze and explore the Boston house Price Data 
+#TODO 2:Split your data for training and testing 
+#TODO 3:Run a Multivariable Regression 
+#TODO 4:Evalute how your model's coefficients and residuals 
+#TODO 5:Use Data transformation to improce your model perfomarnce 
+#TODO 6:Use your model to estimate a property price 
+
+import pandas as pd 
+import numpy as np
+
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
